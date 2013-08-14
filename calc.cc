9f716aad04f9ef8f06ef8921f20293cfb5c8101d
A Cripps <adam@monkeez.org>
2013-08-14 13:02:36 +0100

    Adding a new calc.cc file

---
+++ calc.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  int iNumber1, iNumber2;
+  cin >> iNumber1;
+  cin >> iNumber2;
+ 
+  cout << "Number1: " << iNumber1 << "\n"
+       << "Number2: " << iNumber2 << endl;
+ 
+  return 0;
+}
