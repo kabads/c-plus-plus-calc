c8809fba1b7efc3061852b61751c89a65db0f2e5
A Cripps <adam@monkeez.org>
2013-08-14 12:49:43 +0100

    changing input variable to int works - and tidied up previous method

--- hello.cc
+++ hello.cc
@@ -2,14 +2,14 @@
  
 #include <iostream>
  
-void query(){
+/*void query(){
   String doing;
   std::cin>>doing;
 
-}
+  }*/
 // OK - I'm not sure what I'm doing here - I'm declaring doing, but the compiler is saying that it is not declared. I then tried creating a new void method, but that said the same. To me, this error is strange. 
 int main(){
-  String doing;
+  String doing; //declaring this as an int works - why not string? 
   //query();
   std::cout << "Hello World!" << std::endl;
   std::cin >> doing;
