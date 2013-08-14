979fcd4e7ea73a6b93100f2981441a78aed9c50b
A Cripps <adam@monkeez.org>
2013-08-14 12:35:46 +0100

    tried some input

--- hello.cc
+++ hello.cc
@@ -2,8 +2,18 @@
  
 #include <iostream>
  
+void query(){
+  String doing;
+  std::cin>>doing;
+
+}
+// OK - I'm not sure what I'm doing here - I'm declaring doing, but the compiler is saying that it is not declared. I then tried creating a new void method, but that said the same. To me, this error is strange. 
 int main()
 {
+  String doing;
+  //query();
   std::cout << "Hello World!" << std::endl;
+  std::cin>>doing;
   return 0;
 }
+
