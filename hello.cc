b4bcfcd3e4a791e5fa5433a14922b296a2c13a37
A Cripps <adam@monkeez.org>
2013-08-14 14:14:18 +0100

    just added a basic print of a text - although it only returns the first word.

--- hello.cc
+++ hello.cc
@@ -1,7 +1,7 @@
 // 'Hello World!' program 
  
 #include <iostream>
- 
+#include <string> 
 /*void query(){
   String doing;
   std::cin>>doing;
@@ -9,7 +9,7 @@
   }*/
 // OK - I'm not sure what I'm doing here - I'm declaring doing, but the compiler is saying that it is not declared. I then tried creating a new void method, but that said the same. To me, this error is strange. 
 int main(){
-  String doing; //declaring this as an int works - why not string? 
+  //String doing; //declaring this as an int works - why not string? 
 	//Matthew: in C++ all standard library classes use lowercase letters only
 	//To declare it you would write 'string doing'
 	//Also string is declared in its own header so you will need to include <string>
@@ -20,8 +20,10 @@ int main(){
   // So, this means that String is an object and string is a variable? That's the way it works in Java. 
 
   //query();
+  std::string doing;
   std::cout << "Hello World!" << std::endl;
   std::cin >> doing;
+  std::cout << doing << std::endl;
   return 0;
 }
 
