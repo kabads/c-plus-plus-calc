e135f23d97e16740e55413795f02dca5365f52d2
A Cripps <adam@monkeez.org>
2013-08-14 14:10:47 +0100

    a question added

--- hello.cc
+++ hello.cc
@@ -16,6 +16,9 @@ int main(){
 	//As well string in the std namespace so you will need to write std::string
 	//If you get fed up of writing std:: add 'using namespace std;' just below your includes
 		//(this tells the compiler to look in std:: if the name is not found without the std::
+  
+  // So, this means that String is an object and string is a variable? That's the way it works in Java. 
+
   //query();
   std::cout << "Hello World!" << std::endl;
   std::cin >> doing;
