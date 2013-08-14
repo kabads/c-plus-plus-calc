51013d1421ecf1de6640a070a1ba93e3d2e6c0ee
mthinkcpp <matthew@yorkbeach.co.uk>
2013-08-14 15:17:09 +0100

    Notes about objects and variables being the same thing

--- hello.cc
+++ hello.cc
@@ -17,7 +17,16 @@ int main(){
 	//If you get fed up of writing std:: add 'using namespace std;' just below your includes
 		//(this tells the compiler to look in std:: if the name is not found without the std::
   
-  // So, this means that String is an object and string is a variable? That's the way it works in Java. 
+  // So, this means that String is an object and string is a variable? That's the way it works in Java.
+  // The std::string is a 'user defined type', or an object (Note: In C++ all objects behave as variables, unless made constants)
+    // int is a variable. std::string is also a variable (Note: 'String' does not exist).
+    // the Java concept of objects being different from variables does NOT apply in C++, they are the same,
+        //also int has the same rights (i.e. a string could behave as an int)
+            // as std::string, just std::string is not built into the compiler.
+        // it's the same with std::complex (complex number), you use it like an int (all mathematical operators apply)
+        // however std::complex is a type in its own right and int!=complex, and it also lets you access the real and imaginary parts.
+        // It is what I said yesterday about built in types having equal rights and abilities to user/library defined types
+        // Ask if the above doesn't make sense.
 
   //query();
   std::string doing;
