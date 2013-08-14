1c3e7a272a5ba8761a9832e295a74eb6aa2e4c37
mthinkcpp <matthew@yorkbeach.co.uk>
2013-08-14 13:07:42 +0100

    Added string explanation

--- hello.cc
+++ hello.cc
@@ -10,6 +10,12 @@
 // OK - I'm not sure what I'm doing here - I'm declaring doing, but the compiler is saying that it is not declared. I then tried creating a new void method, but that said the same. To me, this error is strange. 
 int main(){
   String doing; //declaring this as an int works - why not string? 
+	//Matthew: in C++ all standard library classes use lowercase letters only
+	//To declare it you would write 'string doing'
+	//Also string is declared in its own header so you will need to include <string>
+	//As well string in the std namespace so you will need to write std::string
+	//If you get fed up of writing std:: add 'using namespace std;' just below your includes
+		//(this tells the compiler to look in std:: if the name is not found without the std::
   //query();
   std::cout << "Hello World!" << std::endl;
   std::cin >> doing;
