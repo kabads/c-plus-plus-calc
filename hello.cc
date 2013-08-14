665c661ddd436601e02c4a63c2e878fcc576b176
A Cripps <adam@monkeez.org>
2013-08-14 17:23:31 +0100

    Just trying different ways of declaring a variable. Not working!

--- hello.cc
+++ hello.cc
@@ -2,12 +2,8 @@
  
 #include <iostream>
 #include <string> 
-/*void query(){
-  String doing;
-  std::cin>>doing;
 
-  }*/
-// OK - I'm not sure what I'm doing here - I'm declaring doing, but the compiler is saying that it is not declared. I then tried creating a new void method, but that said the same. To me, this error is strange. 
+
 int main(){
   //String doing; //declaring this as an int works - why not string? 
 	//Matthew: in C++ all standard library classes use lowercase letters only
@@ -19,18 +15,18 @@ int main(){
   
   // So, this means that String is an object and string is a variable? That's the way it works in Java. 
 
-  //query();
-  int first;
-  int second;
+
+  std::int first;
+  std::int second;
   std::string operator;
-  int answer;
+  std::int answer;
 //std::cout << "Hello World!" << std::endl;
   std::cin << first;
   std::cin << operator;
   std::cin << second;
   if (operator == "+")
-    answer = first + second
-  std::cout << answer << std::endl;
+    answer = first + second;
+  std::cout >> answer << std::endl;
   return 0;
 }
 
